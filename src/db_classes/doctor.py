d0a9253d0f746e4d78edcb7b75199cbb8d3a6298
cesarjakub <cesar@spsejecna.cz>
2024-01-29 20:22:22 +0100

    feat: just some minor fix

--- src/db_classes/doctor.py
+++ src/db_classes/doctor.py
@@ -1,3 +1,9 @@
 class Doctor:
-    def __init__(self):
-        pass
+    def __init__(self, ID=None, specialization_ID=None, First_name=None, Last_name=None, Title=None, Date_of_birth=None, Telephone=None):
+        self.ID = ID
+        self.specialization_ID = specialization_ID
+        self.First_name = First_name
+        self.Last_name = Last_name
+        self.Title = Title
+        self.Date_of_birth = Date_of_birth
+        self.Telephone = Telephone
